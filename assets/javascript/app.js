3bb83c80840336f1a8d44f10f3d20a7947f93aea
stlefebvre <lefebvre.shelby@gmail.com>
2018-04-07 22:14:20 -0400

    Scaffolded HTML, created objects for questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+//Questions
+    yearFounded = {
+        question: "What year were the Boston Bruins founded?",
+        answerChoices: ["1918", "1924", "1920", "1925"],
+        correctAnswer: "2"
+    }
+
+    stanleyCups = {
+        question: "How many Stanley Cups have the Bruins won?",
+        answerChoices: ["5", "4", "6", "3"],
+        correctAnswer: "3"
+    }
+
+    willieOree = {
+        question: "In 1958, the first black professional hockey player in the NHL stepped onto the ice as a right winger for the Bruins. What is his name?",
+        answerChoices: ["Willie O'Ree", "Wayne Simmonds", "PK Subban", "Bill Riley"],
+        correctAnswer: "1"
+    }
+
+    zdenoChara = {
+        question: "Bruins captain Zdeno Chara holds the all-time record for the Hardest Shot event at the NHL All-Star Game. How fast was this shot recorded at?",
+        answerChoices: ["107.8 mph", "108.8 mph", "108.5 mph", "109.1 mph"],
+        correctAnswer: "2"
+    }
+
+    bobbyOrr = {
+        question: "It goes without saying that Bruins legend Bobby Orr was the greatest player of all time. Among his many records, he still holds the all-time record for Plus-Minus by a single player in one single season. What was his Plus-Minus?",
+        answerChoices: ["+129", "+120", "+126", "+124"],
+        correctAnswer: "4"
+    }
+
+    allQuestions = [yearFounded, stanleyCups, willieOree, zdenoChara, bobbyOrr]
